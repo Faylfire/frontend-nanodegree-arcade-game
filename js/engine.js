394e0ed8bc77768542f9fbcfb0c26b6121a3af60
Faylfire <fanglu@Fangs-MacBook-Pro.local>
2016-08-12 21:00:16 -0500

    feat: Add footer to HTML for github repo link
    index.html
    ———————
     * Add <hr> tag to break up the main content and footer
     * Add footer section containing github icon and link to github repo
    for FE nano degree arcade game.

--- js/engine.js
+++ js/engine.js
@@ -29,7 +29,9 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    var gCont = doc.querySelector("#gameContainer");
+    //doc.body.appendChild(canvas);
+    gCont.appendChild(canvas);
 
 
     /* This function serves as the kickoff point for the game loop itself
